1ffc7aee8afabc4167b0a6cc7476b4b00804e30c
ludik <bakhanetssimon@gmail.com>
2025-12-31 00:01:19 +0700

    Initial Commit

---
+++ VaTask Mingle/VaTask_MingleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaTask_MingleApp.swift
+//  VaTask Mingle
+//
+//  Created by Simon Bakhanets on 31.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct VaTask_MingleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VaTask MingleTests/VaTask_MingleTests.swift
@@ -0,0 +1,16 @@
+//
+//  VaTask_MingleTests.swift
+//  VaTask MingleTests
+//
+//  Created by Simon Bakhanets on 31.12.2025.
+//
+
+import Testing
+
+struct VaTask_MingleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
